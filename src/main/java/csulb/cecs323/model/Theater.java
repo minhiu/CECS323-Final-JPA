8a8eb5888d7c482d85c52ccf9164f12cf0d1ff39
hhnguyen91 <47880450+hhnguyen91@users.noreply.github.com>
2020-04-30 21:55:56 -0700

    Theater class

---
+++ src/main/java/csulb/cecs323/model/Theater.java
@@ -0,0 +1,78 @@
+package csulb.cecs323.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "theater")
+public class Theater {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private String name;
+    private String city;
+    private String region;
+    private Enumerated country;
+    private int numberOfScreens;
+    private String phone;
+
+    public Theater(){}
+
+    public Theater(String name, String city, String region, Enumerated country, int numberOfScreens, String phone)
+    {
+        this.name=name;
+        this.city=city;
+        this.region=region;
+        this.country=country;
+        this.numberOfScreens=numberOfScreens;
+        this.phone=phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name= name;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public Enumerated getCountry(){
+        return country;
+    }
+
+    public void setCountry(Enumerated country) {
+        this.country = country;
+    }
+
+    public int getNumberOfScreens() {
+        return numberOfScreens;
+    }
+
+    public void setNumberOfScreens(int numberOfScreens) {
+        this.numberOfScreens = numberOfScreens;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
