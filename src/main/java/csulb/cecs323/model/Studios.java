8ed557e4cf5e6da26b1898b015cb1b57947f101b
Matthew Ortiz <matthewo22@users.noreply.github.com>
2020-04-30 22:01:10 -0700

    Updata Studios.java
    Initial commit of Studios class.

---
+++ src/main/java/csulb/cecs323/model/Studios.java
@@ -0,0 +1,33 @@
+package csulb.cecs323.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="studios")
+public class Studios {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studioID;
+    private String name;
+    private String country;
+
+    public Studios(){
+    }
+
+    public Studios(long studioID, String name, String country){
+        this.studioID = studioID;
+        this.name = name;
+        this.country = country;
+    }
+
+    public Long getStudioID() { return studioID; }
+
+    public void setStudioID(Long studioID) { this.studioID = studioID; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getCountry() { return country; }
+
+    public void setCountry(String country) { this.country = country; }
+}
